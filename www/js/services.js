ff928eebb4da0ee14258c3e832fc7a42749ec5bd
Matt Spaulding <mrmattspaulding@gmail.com>
2016-03-09 05:25:58 -0500

    Added Tina and Helga tracks.

--- www/js/services.js
+++ www/js/services.js
@@ -93,8 +93,8 @@ angular.module('starter.services', ["ionic", "ngCordova"])
             level: 'Moderate',
             caughtUpMessage0: 'Take a break.',
             caughtUpMessage1: 'I will nag you in a bit.',
-            caughtUpMessage2: 'I have to make snacks for the kids.',
-            getToWorkMessage0: 'Get to work.',
+            caughtUpMessage2: 'I have to make snacks for the kiddos.',
+            getToWorkMessage0: 'You got this!',
             getToWorkMessage1: '',
             getToWorkMessage2: 'You can do it!',
             nags: [{
@@ -155,7 +155,7 @@ angular.module('starter.services', ["ionic", "ngCordova"])
                 video: 'donkey-kicks',
                 day: 1,
                 hour: 9,
-                minute: 0
+                minute: 1
             }, {
                 title: 'Touch your toes 30 sec.',
                 message: 'Stretching is important.',
@@ -185,11 +185,25 @@ angular.module('starter.services', ["ionic", "ngCordova"])
                 hour: 8,
                 minute: 26
             }, {
+                title: '20 Donkey Kicks',
+                message: 'Yee Haw.',
+                video: 'donkey-kicks',
+                day: 3,
+                hour: 17,
+                minute: 15
+            }, {
+                title: '40 sec Toe Touch',
+                message: 'Streeeeeeeeetch.',
+                video: 'toe-touch',
+                day: 4,
+                hour: 8,
+                minute: 26
+            }, {
                 title: '15 Squat Jumps',
                 message: 'End it with a bang.',
                 video: 'squat-jumps',
                 popup: 3,
-                day: 3,
+                day: 4,
                 hour: 17,
                 minute: 15
             }, {
@@ -207,61 +221,430 @@ angular.module('starter.services', ["ionic", "ngCordova"])
             screenName: 'TinaTush',
             face: 'img/TinaTush.jpg',
             level: 'Advanced',
-            caughtUpMessage0: 'Take a break.',
-            caughtUpMessage1: 'I will nag you in a bit.',
-            caughtUpMessage2: 'I have to change the litter box anyway.',
-            getToWorkMessage0: 'Get to work.',
-            getToWorkMessage1: 'Or don\'t.',
-            getToWorkMessage2: 'Whatever.',
+            caughtUpMessage0: 'Get some rest.',
+            caughtUpMessage1: 'You will need it.',
+            caughtUpMessage2: '',
+            getToWorkMessage0: 'Push it.',
+            getToWorkMessage1: '',
+            getToWorkMessage2: 'Push it real good.',
             nags: [{
+                title: 'Hey there, I\'m Tina',
+                message: 'And I will be nagging you.',
+                details: 'I am a college student majoring in getting your butt in shape.',
+                done: "Let\'s do it!",
+                day: 0,
+                hour: 0,
+                minute: 0
+            }, {
+                title: '25 Squats',
+                message: 'Let\'s start with some squats.',
+                details: 'Make sure you are doing them properly.',
+                video: 'squats',
+                day: 0,
+                hour: 0,
+                minute: 0
+            }, {
+                title: '25 Lunges',
+                message: 'And then some lunges.',
+                video: 'lunges',
+                day: 0,
+                hour: 0,
+                minute: 0
+            }, {
+                title: '10 Squat Jumps',
+                message: 'Get some air.',
+                video: 'squat-jumps',
+                popup: 1,
+                day: 0,
+                hour: 0,
+                minute: 0
+            }, {
+                title: 'That\'s it',
+                message: 'Day 1 done.',
+                details: 'I have to get to class. See you tomorrow.',
+                face: 'img/TinaTush.jpg',
+                done: "See you then",
+                popupId: 1,
+                popup: 2,
+                day: 990,
+                hour: 0,
+                minute: 0
+            }, {
                 title: 'Hello',
-                message: 'Welcome to Squat Nagger!',
-                day: 0,
+                message: 'Checking in.',
+                details: 'Tina starts off strong. See you later.',
+                face: 'img/MarinProfile.jpg',
+                done: "See Ya",
+                popupId: 2,
+                day: 990,
                 hour: 0,
                 minute: 0
             }, {
-                title: '15 Squats',
-                message: 'Let\'s do some squats.',
-                day: 0,
-                hour: 0,
+                title: '20 Sumo Squats',
+                message: 'Time to wake up.',
+                video: 'sumo-squats',
+                day: 1,
+                hour: 8,
                 minute: 0
             }, {
-                title: '20 Squats',
-                message: 'Let\'s burn it.',
-                day: 0,
+                title: '20 Donkey Kicks',
+                message: 'Kick it.',
+                video: 'donkey-kicks',
+                day: 1,
+                hour: 9,
+                minute: 1
+            }, {
+                title: 'Touch your toes 30 sec.',
+                message: 'Stretching is good.',
+                video: 'toe-touch',
+                day: 2,
+                hour: 8,
+                minute: 30
+            }, {
+                title: 'Quad stretch 30 sec.',
+                message: 'Cramps are bad.',
+                video: 'quad-stretch',
+                day: 2,
+                hour: 8,
+                minute: 31
+            }, {
+                title: '30 Squat Pulses',
+                message: 'Burn off that dinner.',
+                video: 'squat-pulses',
+                day: 2,
+                hour: 19,
+                minute: 30
+            }, {
+                title: '40 sec Quad Stretch',
+                message: 'Streeeeeeeeetch.',
+                video: 'quad-stretch',
+                day: 3,
+                hour: 8,
+                minute: 26
+            }, {
+                title: '20 Lunges',
+                message: 'Step deep.',
+                video: 'lunges',
+                day: 3,
+                hour: 17,
+                minute: 15
+            }, {
+                title: '40 sec Quad Stretch',
+                message: 'Streeeeeeeeetch.',
+                video: 'quad-stretch',
+                day: 4,
+                hour: 8,
+                minute: 26
+            }, {
+                title: '20 Curtsey Lunges',
+                message: 'Like a lady.',
+                video: 'curtsey-lunges',
+                day: 4,
+                hour: 17,
+                minute: 15
+            }, {
+                title: '40 sec Toe Touch',
+                message: 'Streeeeeeeeetch.',
+                video: 'toe-touch',
+                day: 4,
+                hour: 8,
+                minute: 26
+            }, {
+                title: '20 Curtsey Lunges',
+                message: 'Like a lady.',
+                video: 'curtsey-lunges',
+                day: 4,
+                hour: 17,
+                minute: 15
+            }, {
+                title: '40 sec Quad Stretch',
+                message: 'Streeeeeeeeetch.',
+                video: 'quad-stretch',
+                day: 5,
+                hour: 8,
+                minute: 26
+            }, {
+                title: '20 Donkey Kicks',
+                message: 'Yee Haw.',
+                video: 'donkey-kicks',
+                day: 5,
+                hour: 12,
+                minute: 15
+            }, {
+                title: '40 sec Quad Stretch',
+                message: 'Streeeeeeeeetch.',
+                video: 'quad-stretch',
+                day: 5,
+                hour: 8,
+                minute: 26
+            }, {
+                title: '30 Squat Pulses',
+                message: 'Pump it.',
+                video: 'squat-pulses',
+                day: 5,
+                hour: 12,
+                minute: 15
+            }, {
+                title: '40 sec Toe Touch',
+                message: 'Streeeeeeeeetch.',
+                video: 'toe-touch',
+                day: 6,
+                hour: 8,
+                minute: 26
+            }, {
+                title: '15 Squat Jumps',
+                message: 'Last day.',
+                video: 'squat-jumps',
+                day: 6,
+                hour: 8,
+                minute: 18
+            }, {
+                title: '30 Squat Pulses',
+                message: 'Pulse.',
+                video: 'squat-pulses',
+                day: 6,
+                hour: 17,
+                minute: 15
+            }, {
+                title: '15 Lunges',
+                message: '',
+                video: 'lunges',
+                day: 6,
+                hour: 17,
+                minute: 16
+            }, {
+                title: '15 Curtsey Lunges',
+                message: '',
+                video: 'curtsey-lunges',
+                popup: 3,
+                day: 6,
+                hour: 17,
+                minute: 17
+            }, {
+                title: 'Congrats',
+                message: 'You did it!',
+                details: 'You made it through the Tina Tush track. Try the Helga Hindquarters track next.',
+                face: 'img/Trophy.jpg',
+                popupId: 3,
+                day: 990,
                 hour: 0,
                 minute: 0
             }]
+
         }, {
             name: 'Helga Hindquarters',
             screenName: 'HelgaHindquarters',
             face: 'img/HelgaHindquarters.jpg',
             level: 'Extreme',
-            caughtUpMessage0: 'Take a break.',
-            caughtUpMessage1: 'Let your glutes recover.',
-            caughtUpMessage2: 'You will need the rest.',
-            getToWorkMessage0: 'Get to work.',
-            getToWorkMessage1: 'Or don\'t.',
-            getToWorkMessage2: 'Whatever.',
+            caughtUpMessage0: 'Take break.',
+            caughtUpMessage1: 'Let glutes recover.',
+            caughtUpMessage2: 'You will need rest.',
+            getToWorkMessage0: 'Puny weekling',
+            getToWorkMessage1: 'get off butt.',
+            getToWorkMessage2: 'Helga SMASH!',
             nags: [{
-                title: 'Hello',
-                message: 'Welcome to Squat Nagger!',
+                title: 'Helga SMASH!',
+                message: 'Puny weakling, get off butt.',
+                details: 'Helga make strong butt.',
+                done: "SMASH!",
                 day: 0,
                 hour: 0,
                 minute: 0
             }, {
-                title: '15 Squats',
-                message: 'Let\'s do some squats.',
+                title: '50 Squats',
+                message: 'Squat.',
+                video: 'squats',
                 day: 0,
-                hour: 16,
+                hour: 0,
+                minute: 0
+            }, {
+                title: '50 Lunges',
+                message: 'Lunge.',
+                video: 'lunges',
+                day: 0,
+                hour: 0,
+                minute: 0
+            }, {
+                title: '30 Squat Jumps',
+                message: 'Work.',
+                video: 'squat-jumps',
+                popup: 1,
+                day: 0,
+                hour: 0,
+                minute: 0
+            }, {
+                title: 'That is all today',
+                message: 'Day 1 done.',
+                details: 'Helga must rest.',
+                face: 'img/HelgaHindquarters.jpg',
+                done: "Yes maam",
+                popupId: 1,
+                popup: 2,
+                day: 990,
+                hour: 0,
+                minute: 0
+            }, {
+                title: 'Hello',
+                message: 'Checking in.',
+                details: 'Helga is a bit extreme, but this is what you asked for.',
+                face: 'img/MarinProfile.jpg',
+                done: "See Ya",
+                popupId: 2,
+                day: 990,
+                hour: 0,
+                minute: 0
+            }, {
+                title: '40 Sumo Squats',
+                message: 'Time to wake up.',
+                video: 'sumo-squats',
+                day: 1,
+                hour: 8,
+                minute: 0
+            }, {
+                title: '40 Donkey Kicks',
+                message: 'Kick it.',
+                video: 'donkey-kicks',
+                day: 1,
+                hour: 9,
+                minute: 1
+            }, {
+                title: 'Touch your toes 60 sec.',
+                message: '',
+                video: 'toe-touch',
+                day: 2,
+                hour: 8,
+                minute: 30
+            }, {
+                title: 'Quad stretch 60 sec.',
+                message: '',
+                video: 'quad-stretch',
+                day: 2,
+                hour: 8,
+                minute: 31
+            }, {
+                title: '60 Squat Pulses',
+                message: '',
+                video: 'squat-pulses',
+                day: 2,
+                hour: 19,
+                minute: 30
+            }, {
+                title: '60 sec Quad Stretch',
+                message: '',
+                video: 'quad-stretch',
+                day: 3,
+                hour: 8,
                 minute: 26
             }, {
-                title: '20 Squats',
-                message: 'Let\'s burn it.',
-                day: 0,
-                hour: 16,
-                minute: 27
+                title: '40 Lunges',
+                message: 'Step deep.',
+                video: 'lunges',
+                day: 3,
+                hour: 17,
+                minute: 15
+            }, {
+                title: '60 sec Quad Stretch',
+                message: '',
+                video: 'quad-stretch',
+                day: 4,
+                hour: 8,
+                minute: 26
+            }, {
+                title: '40 Curtsey Lunges',
+                message: 'Like a lady.',
+                video: 'curtsey-lunges',
+                day: 4,
+                hour: 17,
+                minute: 15
+            }, {
+                title: '60 sec Toe Touch',
+                message: '',
+                video: 'toe-touch',
+                day: 4,
+                hour: 8,
+                minute: 26
+            }, {
+                title: '40 Curtsey Lunges',
+                message: '',
+                video: 'curtsey-lunges',
+                day: 4,
+                hour: 17,
+                minute: 15
+            }, {
+                title: '60 sec Quad Stretch',
+                message: '',
+                video: 'quad-stretch',
+                day: 5,
+                hour: 8,
+                minute: 26
+            }, {
+                title: '40 Donkey Kicks',
+                message: '',
+                video: 'donkey-kicks',
+                day: 5,
+                hour: 12,
+                minute: 15
+            }, {
+                title: '60 sec Quad Stretch',
+                message: '',
+                video: 'quad-stretch',
+                day: 5,
+                hour: 8,
+                minute: 26
+            }, {
+                title: '60 Squat Pulses',
+                message: 'Pump it.',
+                video: 'squat-pulses',
+                day: 5,
+                hour: 12,
+                minute: 15
+            }, {
+                title: '60 sec Toe Touch',
+                message: '',
+                video: 'toe-touch',
+                day: 6,
+                hour: 8,
+                minute: 26
+            }, {
+                title: '30 Squat Jumps',
+                message: 'Last day.',
+                video: 'squat-jumps',
+                day: 6,
+                hour: 8,
+                minute: 18
+            }, {
+                title: '60 Squat Pulses',
+                message: 'Pulse.',
+                video: 'squat-pulses',
+                day: 6,
+                hour: 17,
+                minute: 15
+            }, {
+                title: '30 Lunges',
+                message: '',
+                video: 'lunges',
+                day: 6,
+                hour: 17,
+                minute: 16
+            }, {
+                title: '30 Curtsey Lunges',
+                message: '',
+                video: 'curtsey-lunges',
+                popup: 3,
+                day: 6,
+                hour: 17,
+                minute: 17
+            }, {
+                title: 'Congrats',
+                message: 'You did it!',
+                details: 'You defeated Helga Hindquarters!',
+                face: 'img/Trophy.jpg',
+                popupId: 3,
+                day: 990,
+                hour: 0,
+                minute: 0
             }]
+
         }
     ];
 
