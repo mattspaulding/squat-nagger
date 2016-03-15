63de153faac2135019a7560a5f3d46e97d63a72a
Matt Spaulding <mrmattspaulding@gmail.com>
2016-03-15 01:23:49 -0400

    Remove whitelist.

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -41,8 +41,7 @@ module.exports.metadata =
 {
     "cordova-plugin-app-event": "1.1.0",
     "cordova-plugin-device": "1.1.1",
-    "de.appplant.cordova.plugin.local-notification": "0.8.4",
-    "cordova-plugin-whitelist": "1.2.2-dev"
+    "de.appplant.cordova.plugin.local-notification": "0.8.4"
 }
 // BOTTOM OF METADATA
 });
--- platforms/ios/platform_www/cordova_plugins.js
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -41,8 +41,7 @@ module.exports.metadata =
 {
     "cordova-plugin-device": "1.1.1",
     "cordova-plugin-app-event": "1.1.0",
-    "de.appplant.cordova.plugin.local-notification": "0.8.4",
-    "cordova-plugin-whitelist": "1.2.2-dev"
+    "de.appplant.cordova.plugin.local-notification": "0.8.4"
 }
 // BOTTOM OF METADATA
 });
