767b02a3b7e0fe390b0907fe03dbd8b54bd56ee6
Matt Spaulding <mattspaulding@Matts-MBP.home>
2016-04-03 21:14:31 -0400

    Trying to get app store to accept it.

--- platforms/ios/Squat Nagger/main.m
+++ platforms/ios/Squat Nagger/main.m
@@ -18,7 +18,7 @@
  */
 //
 //  main.m
-//  HelloCordova
+//  Squat Nagger
 //
 //  Created by ___FULLUSERNAME___ on ___DATE___.
 //  Copyright ___ORGANIZATIONNAME___ ___YEAR___. All rights reserved.
--- www/lib/ionic-platform-web-client/dist/ionic.io.bundle.js
+++ www/lib/ionic-platform-web-client/dist/ionic.io.bundle.js
@@ -3912,7 +3912,7 @@ var BaseSettings = (function () {
 })();
 
 var temp = new BaseSettings().factory('$ionicCoreSettings', function () {
-  "IONIC_SETTINGS_STRING_START";var settings = {"app_id":"87b7f3e5","api_key":"fa0ed8e7669f7d72eeecbff009bf94a1a31aa162900bd298","dev_push":true}; return { get: function(setting) { if (settings[setting]) { return settings[setting]; } return null; } };"IONIC_SETTINGS_STRING_END";
+  "IONIC_SETTINGS_STRING_START";var settings = {"app_id":"87b7f3e5","api_key":"fa0ed8e7669f7d72eeecbff009bf94a1a31aa162900bd298"}; return { get: function(setting) { if (settings[setting]) { return settings[setting]; } return null; } };"IONIC_SETTINGS_STRING_END";
 }).finish();
 
 var Settings = (function () {
